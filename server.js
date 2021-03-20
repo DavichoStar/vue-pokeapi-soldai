8ab7c061699d9328048490512147b26f06932f4f
DavichoStar <erickdavid_estrella@hotmail.com>
2021-03-19 22:42:48 -0600

    cors add

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const serveStatic = require("serve-static");
 const path = require("path");
+const cors = require("cors");
 const app = express();
 
-app.use(serveStatic(path.join(__dirname, "dist")));
+app.use(cors()).use(serveStatic(path.join(__dirname, "dist")));
 const port = process.env.PORT || 80;
 
 app.listen(port);
